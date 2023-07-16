299ac5d692362fc11238b8c58bbaa06f9c8e67c0
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-07-16 19:02:09 -0400

    Improved insertNewHTML function

--- js/viewport-handler.js
+++ js/viewport-handler.js
@@ -1,17 +1,27 @@
 import { mobileSiteHTML, tabletSiteHTML } from "./mobile-tablet-markups.js";
 
+// FLAGS
+let isMobileInserted = false;
+let isTabletInserted = false;
+
+// EVENT LISTENER CALLBACK FUNCTION
 function insertNewHTML() {
   const viewportWidth =
     window.innerWidth || document.documentElement.clientWidth;
   const mainContainer = document.getElementsByTagName("main")[0];
 
-  if (viewportWidth <= 767) {
+  if (viewportWidth <= 767 && !isMobileInserted) {
     mainContainer.innerHTML = mobileSiteHTML;
-  } else {
+    isMobileInserted = true;
+    isTabletInserted = false;
+  } else if (viewportWidth > 767 && !isTabletInserted) {
     mainContainer.innerHTML = tabletSiteHTML;
+    isTabletInserted = true;
+    isMobileInserted = false;
   }
 }
 
 insertNewHTML();
 
+// EVENT LISTENER
 window.addEventListener("resize", insertNewHTML);
